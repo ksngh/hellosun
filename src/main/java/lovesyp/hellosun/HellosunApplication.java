b404790e0f473ef19de286df7cdb70a809614c8e
ksngh <chzhqk98@naver.com>
2024-01-09 21:03:05 +0900

    initial commit

---
+++ src/main/java/lovesyp/hellosun/HellosunApplication.java
@@ -0,0 +1,13 @@
+package lovesyp.hellosun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellosunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellosunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lovesyp/hellosun/HellosunApplicationTests.java
@@ -0,0 +1,13 @@
+package lovesyp.hellosun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellosunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
